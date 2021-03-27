cffc3fdac161a0931cae9051ae7437109d91e63e
OleksiiYesenin1983 <oleksii.yesenin@onlini.co>
2021-03-27 11:21:44 +0200

    Login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
+import { LoginComponent } from './components/login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'portal',
+    pathMatch: 'full'
+  },
+  {
+    path: 'portal',
+    component: LoginComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
